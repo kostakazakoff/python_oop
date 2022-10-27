9d30bb302d7162101becd007a8adb06edebce333
Kosta Kazakov <kostakazakoff@gmail.com>
2022-10-27 12:59:52 +0300

    code optimising

--- first_steps_in_oop/hero.py
+++ first_steps_in_oop/hero.py
@@ -11,11 +11,3 @@ class Hero:
 
     def heal(self, amount):
         self.health += amount
-
-
-
-hero = Hero("Peter", 100)
-print(hero.defend(50))
-hero.heal(50)
-print(hero.defend(99))
-print(hero.defend(1))
