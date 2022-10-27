ac26a7047f6ecef653875d45bb7117abb421919e
Kosta Kazakov <kostakazakoff@gmail.com>
2022-10-27 22:49:53 +0300

    Add "Pokemon Battle" project

---
+++ first_steps_in_oop/project/trainer.py
@@ -0,0 +1,36 @@
+from pokemon import Pokemon
+
+
+class Trainer:
+    def __init__(self, name: str):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if pokemon not in self.pokemons:
+            self.pokemons.append(pokemon)
+            return f"Caught {pokemon.pokemon_details()}"
+        return "This pokemon is already caught"
+
+    def release_pokemon(self, pokemon_name: str):
+        pokemon = [p for p in self.pokemons if p.name == pokemon_name]
+        if pokemon:
+            self.pokemons.remove(pokemon[0])
+            return f"You have released {pokemon_name}"
+        return "Pokemon is not caught"
+
+    def trainer_data(self):
+        pokemons_details = '\n'.join([f'- {x.pokemon_details()}' for x in self.pokemons])
+        return f'Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}\n{pokemons_details}'
+
+
+pokemon = Pokemon("Pikachu", 90)
+print(pokemon.pokemon_details())
+trainer = Trainer("Ash")
+print(trainer.add_pokemon(pokemon))
+second_pokemon = Pokemon("Charizard", 110)
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.trainer_data())
