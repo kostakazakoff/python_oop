7edfa57e770ece8349e9d230a050cffd75f7a356
Kosta Kazakov <kostakazakoff@gmail.com>
2022-11-19 20:37:06 +0200

    "Custom range" problem solved

---
+++ iterators_and_generators/lab/custom_range.py
@@ -0,0 +1,18 @@
+class custom_range:
+    def __init__(self, start, end):
+        self.number = start - 1
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.number == self.end:
+            raise StopIteration
+        self.number += 1
+        return self.number
+
+
+one_to_ten = custom_range(1, 10)
+for num in one_to_ten:
+    print(num)
