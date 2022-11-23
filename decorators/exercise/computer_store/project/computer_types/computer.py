af76460b5be1cc5493e4cc78122963d8667a966b
Kosta Kazakov <kostakazakoff@gmail.com>
2022-11-23 15:35:21 +0200

    "Computer store" added

--- decorators/exercise/computer_store/project/computer_types/computer.py
+++ decorators/exercise/computer_store/project/computer_types/computer.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from math import log2
 
 
 class Computer(ABC):
@@ -17,6 +18,7 @@ class Computer(ABC):
     def manufacturer(self, value):
         if len(value.strip()) == 0:
             raise ValueError("Manufacturer name cannot be empty.")
+
         self.__manufacturer = value
 
     @property
@@ -27,11 +29,26 @@ class Computer(ABC):
     def model(self, value):
         if len(value.strip()) == 0:
             raise ValueError("Model name cannot be empty.")
+            
         self.__model = value
-
-    @abstractmethod
-    def configure_computer(self, processor: str, ram,: int):
-        ...
 
     def __repr__(self) -> str:
         return f"{self.manufacturer} {self.model} with {self.processor} and {self.ram}GB RAM"
+
+    @property
+    @abstractmethod
+    def processors(self):
+        ...
+
+    @property
+    @abstractmethod
+    def max_ram(self):
+        ...
+
+    @abstractmethod
+    def configure_computer(self, processor: str, ram: int):
+        ...
+
+    @staticmethod
+    def ram_is_valid(ram_size):
+        return log2(ram_size) == int(log2(ram_size))
